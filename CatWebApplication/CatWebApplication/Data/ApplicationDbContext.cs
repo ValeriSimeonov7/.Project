f5a519cc88b1ee6cb9ef27223743ccf41c481fd0
ValeriSimeonov7 <v.simeonov2020@pmg-pernik.com>
2024-11-20 08:35:41 +0200

    Initial commit

---
+++ CatWebApplication/CatWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CatWebApplication.Data;
+
+namespace CatWebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<CatWebApplication.Data.Cat>? Cat { get; set; }
+    }
+}
---
+++ CatWebApplication/CatWebApplication/Data/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatWebApplication.Data
+{
+    public class Cat
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; } = null!;
+
+        [Range(0, 10)]
+        public int Age { get; set; }
+        public string Breed { get; set; } = null!;
+        public string Picture { get; set; } = null!;
+    }
+}
